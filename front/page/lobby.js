4e8fe27f680902e391f73d545fe93a33a8842ca0
Antonis Mavrakis <51758172+MercyMePlz@users.noreply.github.com>
2022-01-11 22:26:25 +0200

    updated connection  with lobby

--- front/page/lobby.js
+++ front/page/lobby.js
@@ -57,14 +57,96 @@ $("#request").click(function () {
 });
 
 //Click on join lobby
-var playBtn = document.getElementById("play_btn");
-playBtn.addEventListener("click", function() {
-    $(".main_btns").hide();
+// var playBtn = document.getElementById("play_btn");
+// playBtn.addEventListener("click", function() {
+// $("#play_btn").click(function(){
+//     // $(document).on('play', '#play_btn', function(){
+//         $(".main_btns").hide();
 
-    // $.ajax({
-    //     URL: "aa/adise2021/api/objects/game",
-    //     type: "GET",
-    //     dataType: 'json',
-    //     success: 
-    // })
+roomsWrap = document.querySelector(".main_btns");
+
+function clickFunct(num){
+  
+
+  $.ajax({
+    method: "POST",
+    url: "/aa/adise2021/routre.php/join",
+    dataType: "json",
+    contentType: 'application/json',
+    data: JSON.stringify({id : num}),
+    success: function(){
+      
+      window.location = `/aa/adise2021/front/page/game/game.html?lobbyId=${num}`;
+    },
+    error: function(){
+      alert("Something went wrong :(");
+    }
+  })
+}
+
+$(document).on("click", "#play_btn", function () {
+  $(".btnH").hide();
+
+  var jwt = getCookie('jwt'); 
+  $.ajax({
+    method: "POST",
+    url: "/aa/adise2021/routre.php/getrooms",
+    dataType: "json",
+    data: JSON.stringify({jwt:jwt}),
+    contentType: "application/json",
+    success: function (result) {
+      console.log(result);
+      results = result.rooms;
+
+      var temp = -1;
+
+      for (roomsKey in results) {
+        console.log(results[roomsKey].id);
+
+        if (temp != results[roomsKey].id) {
+          var htmlToAdd = `<div onclick="clickFunct(${results[roomsKey].id})" class="roomContainer">
+              <div class = "roomId">Room Id:  ${results[roomsKey].id} <br> </div>
+              <div class = "participants">Participant 1: ${results[roomsKey].username || ""}`;
+
+          htmlToAdd += `<br></div> <div class = "btnConnect">`;
+          
+          htmlToAdd += `Active<a href="" class="aBtnA aBtn"></a></div>
+          </div>`;
+          
+          temp = results[roomsKey].id
+
+          roomsWrap.innerHTML += htmlToAdd;
+        }else{
+
+          var num = [...document.querySelectorAll('.participants')];
+          var last = num[num.length-1];
+
+          last.innerHTML += `Participant 2: ${results[roomsKey].username}`
+
+          document.querySelectorAll('.roomContainer')[num.length-1].classList.add('unclick');
+
+          document.querySelectorAll('.btnConnect')[num.length-1].innerHTML = `Full<a href="" class="aBtnF aBtn"></a></div> </div>`;
+
+        }
+      }
+    }
+  });
+  
 });
+
+function getCookie(cname){
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+     var c = ca[i];
+     while (c.charAt(0) == ' '){
+       c = c.substring(1);
+     }
+  
+     if (c.indexOf(name) == 0) {
+       return c.substring(name.length, c.length);
+     }
+  }
+  return "";
+  }
