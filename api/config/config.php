23bf05dd25b71ff160da9aa4c02ddbc93f8bbce0
Antonis Mavrakis <51758172+MercyMePlz@users.noreply.github.com>
2022-01-15 01:51:01 +0200

    Update config.php

--- api/config/config.php
+++ api/config/config.php
@@ -15,7 +15,7 @@
 
 if(gethostname()=='users.iee.ihu.gr') {
     $this->conn =new mysqli($this->host, $this->user,$this->pass, $this->db,null,'/home/student/it/2018/it185222/mysql/run/mysql.sock');
-    print_r($conn);
+    print_r($this->conn);
     
 } else {
     $this->conn =new mysqli($this->host, $this->user, $this->pass, $this->db);
