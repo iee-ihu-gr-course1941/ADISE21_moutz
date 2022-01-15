244641bc73b341a5f1f546871c48cfe4313ba2d4
Antonis Mavrakis <51758172+MercyMePlz@users.noreply.github.com>
2022-01-15 19:22:20 +0200

    Update config.php

--- api/config/config.php
+++ api/config/config.php
@@ -16,7 +16,7 @@
             {
                 if(gethostname()=='users.iee.ihu.gr') {
                    
-                    $this->conn =new PDO('mysql:host='.$this->host.';port=null;unix_socket=/home/staff/asidirop/mysql/run/mysql.sock;dbname='.$this->db,$this->user,$this->password);
+                    $this->conn =new PDO('mysql:host=unix_socket=/home/staff/asidirop/mysql/run/mysql.sock;port=null;dbname='.$this->db,$this->user,$this->password);
                     // $this->conn = new mysqli($this->host, $this->user, $this->password, $this->db,null,'/home/staff/asidirop/mysql/run/mysql.sock');
                     // print_r($this->conn);
                 }else{
